27e0180bd59d3a0e4616a2f25eb1336b386bb3d0
Aniket Kaushik <aniket.kaushik@in2ittech.com>
2024-08-12 17:00:44 +0530

    update

--- user-service/userController/src/main/java/com/blog/controller/controller/BlogController.java
+++ user-service/userController/src/main/java/com/blog/controller/controller/BlogController.java
@@ -6,6 +6,7 @@ import com.blog.service.service.blogService.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +23,11 @@ public class BlogController {
     private BlogService blogService;
 
     @PostMapping("/create")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN','SUPER_AUTHOR','MANAGER','EMPLOYEE','TRAINEE')")
     public ResponseEntity<Object> createBlog(@ModelAttribute  BlogCreationRequest blogCreationRequest) throws IOException {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String userName = authentication.getName();
-        Map<String, Object> blog = blogService.createBlog(userName, blogCreationRequest);
-        return ResponseHandler.responseBuilder("Blog Created SuccessFully", HttpStatus.OK,blog);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String userName = authentication.getName();
+            Map<String, Object> blog = blogService.createBlog(userName, blogCreationRequest);
+            return ResponseHandler.responseBuilder("Blog Created SuccessFully", HttpStatus.OK, blog);
     }
 }
