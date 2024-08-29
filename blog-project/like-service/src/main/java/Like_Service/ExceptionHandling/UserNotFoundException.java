515ec3c497f9e6b39f05182bbcefdde59eaa4389
vanshikaarana <vanshika.pundir08@gmail.com>
2024-08-29 21:06:32 +0530

    made changes in user entity

--- blog-project/like-service/src/main/java/Like_Service/ExceptionHandling/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package Like_Service.ExceptionHandling;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
