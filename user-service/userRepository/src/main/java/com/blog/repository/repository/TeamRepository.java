374476cc9f58a9dbcb650cb197b96060d1e1cee1
Shubham Rathee <shubham.rathee@in2ittech.com>
2024-08-29 11:04:11 +0530

    commented out team from everycode

--- user-service/userRepository/src/main/java/com/blog/repository/repository/TeamRepository.java
+++ user-service/userRepository/src/main/java/com/blog/repository/repository/TeamRepository.java
@@ -1,41 +1,41 @@
-package com.blog.repository.repository;
-
-import com.blog.repository.entity.Team;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface TeamRepository extends JpaRepository<Team, Long> {
-
-    Optional<Team> findByName(String name);
-
-    @Query("SELECT t FROM Team t WHERE t.deletedAt IS NULL")
-    Page<Team> findAllActive(Pageable pageable);
-
-    @Query("SELECT t FROM Team t WHERE t.deletedAt IS NOT NULL")
-    List<Team> findAllDeleted();
-
-    @Query("SELECT t FROM Team t WHERE t.name LIKE %:name% AND t.deletedAt IS NULL")
-    Page<Team> findByNameContainingIgnoreCaseActive(@Param("name") String name, Pageable pageable);
-
-    @Query("SELECT t FROM Team t WHERE t.id = :id AND t.deletedAt IS NULL")
-    Optional<Team> findByIdActive(@Param("id") Long id);
-
-    @Modifying
-    @Query("UPDATE Team t SET t.deletedAt = :deletedAt, t.deletedBy = :deletedBy WHERE t.id = :id AND t.deletedAt IS NULL")
-    void softDelete(@Param("id") Long id, @Param("deletedBy") String deletedBy, @Param("deletedAt") LocalDateTime deletedAt);
-
-    @Modifying
-    @Query("UPDATE Team t SET t.deletedAt = NULL, t.deletedBy = NULL WHERE t.id = :id AND t.deletedAt IS NOT NULL")
-    void restore(@Param("id") Long id);
-
-}
+//package com.blog.repository.repository;
+//
+//import com.blog.repository.entity.Team;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Repository
+//public interface TeamRepository extends JpaRepository<Team, Long> {
+//
+//    Optional<Team> findByName(String name);
+//
+//    @Query("SELECT t FROM Team t WHERE t.deletedAt IS NULL")
+//    Page<Team> findAllActive(Pageable pageable);
+//
+//    @Query("SELECT t FROM Team t WHERE t.deletedAt IS NOT NULL")
+//    List<Team> findAllDeleted();
+//
+//    @Query("SELECT t FROM Team t WHERE t.name LIKE %:name% AND t.deletedAt IS NULL")
+//    Page<Team> findByNameContainingIgnoreCaseActive(@Param("name") String name, Pageable pageable);
+//
+//    @Query("SELECT t FROM Team t WHERE t.id = :id AND t.deletedAt IS NULL")
+//    Optional<Team> findByIdActive(@Param("id") Long id);
+//
+//    @Modifying
+//    @Query("UPDATE Team t SET t.deletedAt = :deletedAt, t.deletedBy = :deletedBy WHERE t.id = :id AND t.deletedAt IS NULL")
+//    void softDelete(@Param("id") Long id, @Param("deletedBy") String deletedBy, @Param("deletedAt") LocalDateTime deletedAt);
+//
+//    @Modifying
+//    @Query("UPDATE Team t SET t.deletedAt = NULL, t.deletedBy = NULL WHERE t.id = :id AND t.deletedAt IS NOT NULL")
+//    void restore(@Param("id") Long id);
+//
+//}
--- user-service/userService/src/main/java/com/blog/service/exceptions/TeamNotFoundException.java
+++ user-service/userService/src/main/java/com/blog/service/exceptions/TeamNotFoundException.java
@@ -1,13 +1,13 @@
-package com.blog.service.exceptions;
-
-import com.blog.repository.entity.Team;
-
-public class TeamNotFoundException extends RuntimeException {
-    public TeamNotFoundException(String s) {
-        super(s);
-    }
-
-    public TeamNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
+//package com.blog.service.exceptions;
+//
+//import com.blog.repository.entity.Team;
+//
+//public class TeamNotFoundException extends RuntimeException {
+//    public TeamNotFoundException(String s) {
+//        super(s);
+//    }
+//
+//    public TeamNotFoundException(String message, Throwable cause) {
+//        super(message, cause);
+//    }
+//}
