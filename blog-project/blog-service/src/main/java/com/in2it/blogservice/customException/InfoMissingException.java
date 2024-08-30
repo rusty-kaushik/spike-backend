d0c4db9926efe0af8498f5a8960df2eb5e8e8ff2
Vaseem Akhtar <vaseem.akhtar@in2ittech.com>
2024-08-30 18:19:41 +0530

    [DEL] deleted some file

--- blog-project/blog-service/src/main/java/com/in2it/blogservice/customException/InfoMissingException.java
+++
@@ -1,9 +0,0 @@
-package com.in2it.blogservice.customException;
-
-@SuppressWarnings("serial")
-public class InfoMissingException extends RuntimeException{
-
-	public InfoMissingException(String msg) {
-		super(msg);
-	}
-}
