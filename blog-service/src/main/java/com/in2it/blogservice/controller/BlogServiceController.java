1be87a1514c3e40115f32ce1fe30bee27264e50f
Vaseem Akhtar <vaseem.akhtar@in2ittech.com>
2024-09-05 13:23:18 +0530

    [RMV] removed log file

--- blog-service/src/main/java/com/in2it/blogservice/controller/BlogServiceController.java
+++ blog-service/src/main/java/com/in2it/blogservice/controller/BlogServiceController.java
@@ -42,59 +42,61 @@ public class BlogServiceController {
 	 * This method is used to insert blog in database.
 	 */
 
-	
-	
 	@Tag(name = "post-blog method", description = "This method used to create a blog post. After calling this method its return a map. ")
 	@PostMapping(path = "/posts", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-	public ResponseEntity<ResponseHandler<BlogDto>> saveBlogWithFile(@ModelAttribute BlogDto blogDto) throws IOException {
+	public ResponseEntity<ResponseHandler<BlogDto>> saveBlogWithFile(@ModelAttribute BlogDto blogDto)
+			throws IOException {
 
 		List<MultipartFile> media = blogDto.getMedia();
 
-	
-		
 		BlogDto saveBlogWithFile = serviceImpl.saveBlogWithFile(blogDto, media);
-		
-		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(saveBlogWithFile, "Blog post successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
-		
+
+		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(saveBlogWithFile, "Blog post successfully.",
+				HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
+
 		return ResponseEntity.ok(response);
 
 //		return ResponseEntity<ResponseHandlersaveBlogWithFile, HttpStatus.OK,HttpStatus.OK.value(),"Blog post successfully.")>;
 	}
 
-	
-	
-	
-
 	@PutMapping(path = "/update/{blogId}", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
 	@Operation(summary = "Update an blog", description = "Update an existing blog. Using blog blog id can update blog title and content only.")
-	public  ResponseEntity<ResponseHandler<BlogDto>> updateBlog( @PathVariable UUID blogId, @RequestBody BlogUpdateDto updateDto) {
+	public ResponseEntity<ResponseHandler<BlogDto>> updateBlog(@PathVariable UUID blogId,
+			@RequestBody BlogUpdateDto updateDto) {
 
-		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.updateBlog(updateDto, blogId), "Blog modefy successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
+		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.updateBlog(updateDto, blogId),
+				"Blog modefy successfully.", HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
 		return ResponseEntity.ok(response);
 	}
 
-	
 	@PutMapping("/updateLike")
 	@Operation(summary = "update like count")
-	public  ResponseEntity<ResponseHandler<BlogDto>> updateLike(@RequestParam UUID blogId, @RequestParam Long totalLikeCount) {
-	
-		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.updateLike(totalLikeCount, blogId), "liked", HttpStatus.ACCEPTED, HttpStatus.ACCEPTED.value(),LocalDateTime.now());
-		
+	public ResponseEntity<ResponseHandler<BlogDto>> updateLike(@RequestParam UUID blogId,
+			@RequestParam Long totalLikeCount) {
+
+		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.updateLike(totalLikeCount, blogId),
+				"liked", HttpStatus.ACCEPTED, HttpStatus.ACCEPTED.value(), LocalDateTime.now());
+
 		return ResponseEntity.ok(response);
 	}
 
 	@PutMapping("/updateComment")
 	@Operation(summary = "update comment count")
-	public  ResponseEntity<ResponseHandler<BlogDto>> updateComment(@RequestParam UUID blogId, @RequestParam Long totalCommentCount) {
-		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.updateComment(totalCommentCount, blogId), "commented", HttpStatus.ACCEPTED, HttpStatus.ACCEPTED.value(),LocalDateTime.now());
-		
+	public ResponseEntity<ResponseHandler<BlogDto>> updateComment(@RequestParam UUID blogId,
+			@RequestParam Long totalCommentCount) {
+		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(
+				serviceImpl.updateComment(totalCommentCount, blogId), "commented", HttpStatus.ACCEPTED,
+				HttpStatus.ACCEPTED.value(), LocalDateTime.now());
+
 		return ResponseEntity.ok(response);
 	}
 
 	@DeleteMapping("/deleteByBlogId/{blogId}")
 	@Operation(summary = "delete blog by blogId ,in updatedBy we can pass whose login then store his userid i.e. Admin123")
-	public  ResponseEntity<ResponseHandler<Boolean>> deleteBlog(@PathVariable UUID blogId, @RequestParam String updatedBy) {
-		ResponseHandler<Boolean> response =new ResponseHandler<>(serviceImpl.deleteBlog(blogId, updatedBy), "Your blog has been deleted successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
+	public ResponseEntity<ResponseHandler<Boolean>> deleteBlog(@PathVariable UUID blogId,
+			@RequestParam String updatedBy) {
+		ResponseHandler<Boolean> response = new ResponseHandler<>(serviceImpl.deleteBlog(blogId, updatedBy),
+				"Your blog has been deleted successfully.", HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
 
 		return ResponseEntity.ok(response);
 
@@ -102,19 +104,21 @@ public class BlogServiceController {
 
 	@DeleteMapping("/deleteByTitle/{title}")
 	@Operation(summary = "delete blog by title, in updatedBy we can pass whose login then store his userid i.e. Admin123")
-	public ResponseEntity<ResponseHandler<Boolean>>  deleteBlogBytitle(@PathVariable String title, @RequestParam UUID blogId,@RequestParam String updatedBy) {
-		ResponseHandler<Boolean> response = new ResponseHandler<>(serviceImpl.deleteBlogByTitle(title, blogId,updatedBy), "Your blog has been deleted successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
+	public ResponseEntity<ResponseHandler<Boolean>> deleteBlogBytitle(@PathVariable String title,
+			@RequestParam UUID blogId, @RequestParam String updatedBy) {
+		ResponseHandler<Boolean> response = new ResponseHandler<>(
+				serviceImpl.deleteBlogByTitle(title, blogId, updatedBy), "Your blog has been deleted successfully.",
+				HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
 		return ResponseEntity.ok(response);
 	}
-
-
 
 	// using search technique
 	@GetMapping("/getAll")
 	@Operation(summary = "Get a all Blogs", description = "Returns  all blogs  ")
-	public  ResponseEntity<ResponseHandler<List<BlogDto>>>  getAllBlog(@RequestParam(defaultValue = "0") int pageNum,
-            @RequestParam(defaultValue = "5") int pageSize) {
-		ResponseHandler<List<BlogDto>> response = new ResponseHandler<>(serviceImpl.getBlog(pageNum,pageSize),  "Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
+	public ResponseEntity<ResponseHandler<List<BlogDto>>> getAllBlog(@RequestParam(defaultValue = "0") int pageNum,
+			@RequestParam(defaultValue = "5") int pageSize) {
+		ResponseHandler<List<BlogDto>> response = new ResponseHandler<>(serviceImpl.getBlog(pageNum, pageSize),
+				"Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
 		return ResponseEntity.ok(response);
 	}
 
@@ -122,40 +126,36 @@ public class BlogServiceController {
 
 	@GetMapping("/getByUserId/{userId}")
 	@Operation(summary = "Get a blog by autherId", description = "Returns a Blog as per the autherId.")
-	public  ResponseEntity<ResponseHandler<List<BlogDto>>> getBlogsByAutherId(@PathVariable @Valid String userId) {
-		ResponseHandler<List<BlogDto>> response = new ResponseHandler<>(serviceImpl.getByAutherID(userId),  "Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
-	
-	
+	public ResponseEntity<ResponseHandler<List<BlogDto>>> getBlogsByAutherId(@PathVariable @Valid String userId) {
+		ResponseHandler<List<BlogDto>> response = new ResponseHandler<>(serviceImpl.getByAutherID(userId),
+				"Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
+
 		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping("/getByBlogId/{blogId}")
 	@Operation(summary = "Get a blog by blogId", description = "Returns a blog as per the blogId.")
-	public  ResponseEntity<ResponseHandler<BlogDto>> getBlogById(@PathVariable(value = "blogId") @Valid UUID blogId) {
-		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.getBlogById(blogId),  "Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
+	public ResponseEntity<ResponseHandler<BlogDto>> getBlogById(@PathVariable(value = "blogId") @Valid UUID blogId) {
+		ResponseHandler<BlogDto> response = new ResponseHandler<BlogDto>(serviceImpl.getBlogById(blogId),
+				"Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
 		return ResponseEntity.ok(response);
 	}
 
-	
 	@GetMapping("/getByTitle/{blogTitle}")
 	@Operation(summary = "Get a blogs by blogTitle", description = "Returns a blogs as list by  blogTitle.")
-	public  ResponseEntity<ResponseHandler<List<BlogDto>>> getBlogByTitle(@RequestParam(defaultValue = "0") int pageNum,
-            @RequestParam(defaultValue = "5") int pageSize,@PathVariable(value = "blogTitle") String title) {
-		
-		List<BlogDto> blogTitleWithPage = serviceImpl.getBlogTitleWithPage(pageNum,pageSize,title);
-		
-		if(!blogTitleWithPage.isEmpty()) {
-			ResponseHandler<List<BlogDto>> response = new ResponseHandler<>(blogTitleWithPage, "Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(),LocalDateTime.now());
+	public ResponseEntity<ResponseHandler<List<BlogDto>>> getBlogByTitle(@RequestParam(defaultValue = "0") int pageNum,
+			@RequestParam(defaultValue = "5") int pageSize, @PathVariable(value = "blogTitle") String title) {
+
+		List<BlogDto> blogTitleWithPage = serviceImpl.getBlogTitleWithPage(pageNum, pageSize, title);
+
+		if (!blogTitleWithPage.isEmpty()) {
+			ResponseHandler<List<BlogDto>> response = new ResponseHandler<>(blogTitleWithPage,
+					"Data retrieved successfully.", HttpStatus.OK, HttpStatus.OK.value(), LocalDateTime.now());
 			return ResponseEntity.ok(response);
-		}else {
-			
+		} else {
+
 			throw new InfoMissingException("please write valid info");
 		}
 	}
-	
-	
-	
-	
-
 
 }
