79b449948009ee27517a439eb60ca5d13ab2396d
vanshikaarana <vanshika.pundir08@gmail.com>
2024-09-10 12:29:15 +0530

    removed dependency of blod-service

--- Like-Service/src/main/java/Like_Service/FeignInterface/BlogClient.java
+++ Like-Service/src/main/java/Like_Service/FeignInterface/BlogClient.java
@@ -1,6 +1,9 @@
 package Like_Service.FeignInterface;
 
-import com.in2it.blogservice.dto.BlogDto;
+
+import Like_Service.BlogDto.BlogDto;
+
+
 import com.in2it.blogservice.reponse.ResponseHandler;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.UUID;
 
 //localhost:8282/spike/blog
 @FeignClient(name = "blog-service", url = "${feign.client.url}")
